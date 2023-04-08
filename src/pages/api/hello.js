5d043fa28388981efa0e6bf639703870a54db852
Stephen Ho <wickedcz4a@gmail.com>
2023-04-07 23:01:56 -0700

    working axios request to OpenAI

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from "axios";
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  axios({
+    method: 'post',
+    url: 'https://api.openai.com/v1/chat/completions',
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer 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",
+    },
+    data: {
+      "model": "gpt-3.5-turbo",
+      "messages": [{"role": "user", "content": "Hello!"}]
+    }
+  })
+  .then((response) => {
+    res.status(200).json(response.data);
+  })
 }
+
+
