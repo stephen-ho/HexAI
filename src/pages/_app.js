f3ae90371d347b63dcdfa1c8ee8fb065ea45dadd
Stephen Ho <wickedcz4a@gmail.com>
2023-06-03 16:43:18 -0700

    added google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,8 @@ import { Roboto } from 'next/font/google'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import '@/styles/globals.css'
+import Script from "next/script";
+
 
 const roboto = Roboto({
   weight: '400',
@@ -16,11 +18,29 @@ const darkTheme = createTheme({
 
 export default function App({ Component, pageProps }) {
   return (
+    <>
+    <Script
+      strategy='lazyOnload'
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+    />
+
+    <Script id="analytics" strategy='lazyOnload'>
+      {
+        `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `
+      }
+    </Script>
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <main className={roboto.className}>
         <Component {...pageProps} />
       </main>
     </ThemeProvider>
+    </>
   )
 }
